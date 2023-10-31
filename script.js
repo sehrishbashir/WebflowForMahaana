7c263e1d166bec7c9b8859d452a3dbdb06f0f6c1
Sehrish <sehrish.bashir@mparsec.com>
2023-10-31 12:28:05 +0500

    add helpcrunch script

---
+++ script.js
@@ -0,0 +1,6 @@
+const org = 'mahaanawealth';
+const appId = '3e2db6fc-88c6-4d53-8713-fa639bf8c4f2';
+const userData = {custom_data: {approch: 'Website'}}
+
+window.helpcrunchSettings = { organization: org, appId: appId, user: userData};
+(function(w,d){var hS=w.helpcrunchSettings;if(!hS||!hS.organization){return;}var widgetSrc='https://'+hS.organization+'.widget.helpcrunch.com/';w.HelpCrunch=function(){w.HelpCrunch.q.push(arguments)};w.HelpCrunch.q=[];function r(){if (d.querySelector('script[src="' + widgetSrc + '"')) { return; }var s=d.createElement('script');s.async=1;s.type='text/javascript';s.src=widgetSrc;(d.body||d.head).appendChild(s);}if(d.readyState === 'complete'||hS.loadImmediately){r();} else if(w.attachEvent){w.attachEvent('onload',r)}else{w.addEventListener('load',r,false)}})(window, document)
