efa5d584263f7b0267e8dd9315c5978713ceb3e5
Sehrish <sehrish.bashir@mparsec.com>
2023-11-03 12:33:17 +0500

    comit

--- micfScript.js
+++ micfScript.js
@@ -185,7 +185,7 @@ function renderLoop(data) {
                                 <div class="text-block-37">90 DAYS</div>
                                 <div class="text-block-38">${data.days90 || '-'}</div>
                             </div>
-                            <div class="div-block-100">
+                            <div>
                                 <div class="text-block-37">1Y</div>
                                 <div class="text-block-38">${data.days365 || '-'}</div>
                             </div>
