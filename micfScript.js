99d277b0b666db81d63200fbb87b82f30b74a576
Sehrish <sehrish.bashir@mparsec.com>
2023-11-21 16:42:40 +0500

    commit

--- micfScript.js
+++ micfScript.js
@@ -615,3 +615,8 @@ if (durationContainerNew) {
 
 
 // ---------------------------------------------- //
+
+
+// const kFormatter = (num) => { let formattedNumber; num = Math.abs(num); if (num >= 1000000000) { formattedNumber = (num / 1000000000).toFixed(1).replace(/\.0$/, '') + 'B'; } else if (num >= 1000000) { formattedNumber = (num / 1000000).toFixed(1).replace(/\.0$/, '') + 'M'; } else if (num >= 10000) { formattedNumber = (num / 10000).toFixed(1).replace(/\.0$/, '') + 'K'; } else { formattedNumber = num.toLocaleString(); } return formattedNumber; }
+
+
