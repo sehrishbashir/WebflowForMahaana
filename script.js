cc5b375fb3607ce68d29671e2132dc444d350741
mohtashimwajid <mohtashim.wajid@mparsec.com>
2023-12-29 13:56:25 +0500

    fixes

--- script.js
+++ script.js
@@ -250,8 +250,8 @@ const mixPanelActions = {
     },
 
     joinWaitlistFailed: (props) => {
-        mixpanel.track(mixPannelTrackerEventName.join_waitlist_failed);
         mixpanel.identify(props);
+        mixpanel.track(mixPannelTrackerEventName.join_waitlist_failed);
         // console.log(mixPannelTrackerEventName.join_waitlist_failed)
 
     },
