387e1f9fba3a5bf88fdf70e8db23eae6f17630ff
mohtashimwajid <mohtashim.wajid@mparsec.com>
2023-12-29 12:54:50 +0500

    fixes

--- script.js
+++ script.js
@@ -73,8 +73,8 @@ api_Token = MIXPANEL_API_TOKEN;
 //     } 
 // })(document, window.mixpanel || []);
 
-import mixpanel from 'mixpanel-browser';
-mixpanel.init(MIXPANEL_API_TOKEN, {debug: true, track_pageview: true, persistence: 'localStorage'});
+// import mixpanel from 'mixpanel-browser';
+// mixpanel.init(MIXPANEL_API_TOKEN, {debug: true, track_pageview: true, persistence: 'localStorage'});
  
 // Set this to a unique identifier for the user performing the event.
 // mixpanel.identify('USER_ID')
