78120a8b6d3fc27c6171e5d82978f430c545708f
mohtashimwajid <mohtashim.wajid@mparsec.com>
2024-01-10 16:12:03 +0500

    test

--- micfScript.js
+++ micfScript.js
@@ -499,7 +499,7 @@ async function fetchData() {
             'shahr-e-advisor': fundInfo.shariahAdvisors,
             'custodian': fundInfo.custodian,
             'weightAverageTime': fundInfo.weightedAverageTime,
-            'ytd-mtd-values':`${fundInfo.monthlyTotalExpenseRatioWithoutLevy} % (MTD)\n${fundInfo.yearlyTotalExpenseRatioWithoutLevy} % (YTD)`
+            'ytd-mtd-values':`${fundInfo.monthlyTotalExpenseRatioWithoutLevy} % (MTD)<br>${fundInfo.yearlyTotalExpenseRatioWithoutLevy} % (YTD)`
         };
 
         if (offeringDocumentList.length > 0) {
