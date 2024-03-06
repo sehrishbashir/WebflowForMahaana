0089dd2e95aa22bee8e05dc599531e8b4e5c670b
Muhammad Aqib <muhammad.aqib@mparsec.com>
2024-03-06 18:09:17 +0500

    chnage id

--- miietf.js
+++ miietf.js
@@ -269,9 +269,9 @@ function renderLoop(data) {
 // }
 
 const demoData = {
-    "id": "65dd8ef568447f58938fb300",
+    "id": "65d832484b0efa092190560b",
     "overview": {
-        "name": "Mahaana",
+        "name": "Mahaana a",
         "assetCategory": "MIIETF is a Shariah-compliant equity index fund that primarily invests in the top 30, free float weighted Islamic stocks that have an annual average turnover of more than PKR 10 million. MIIETF provides investors the long term benefits of equity markets. ",
         "question": "What is Mahaana Islamic Index ETF (MIIETF)?",
         "description": "",
