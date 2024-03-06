c2f441038b8de6b146851db3d2ed34b4eb49e62d
Muhammad Aqib <muhammad.aqib@mparsec.com>
2024-03-06 12:25:18 +0500

    real data

--- miietf.js
+++ miietf.js
@@ -277,7 +277,7 @@ async function fetchData() {
         const dataJson = await response.json()
         console.log( dataJson, "opop =>1")
 
-        const data = demoData;
+        const data = dataJson;
         const { offeringDocumentList, fmrDate, fundInfo, monthToDateExpense, overview, creditRating, assetAllocation, holding } = data;
         let fmrDateElement = document.querySelectorAll('body #fmrDate');
         Array.from(fmrDateElement).forEach(element => { element.textContent = "as of" + " " + moment(fmrDate, 'YYYY-MM-DD').format('D MMM YYYY') });
