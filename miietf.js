9668a7e8248960ee00701a03ce3d0b9ab08ab870
Muhammad Aqib <muhammad.aqib@mparsec.com>
2024-03-07 16:57:43 +0500

    margket

--- miietf.js
+++ miietf.js
@@ -303,7 +303,7 @@ async function fetchData() {
             'fundStabilityRating': fundInfo.fundStabilityRating,
             'authorizedParticipant': fundInfo.authorizedParticipant,
             'i-nav': `${overview.navPerUnit.includes('.') ? Number(overview.navPerUnit).toFixed(4) : Number(overview.navPerUnit)}`,
-            'market': '0',
+            'market': `${(overview?.marketPrice && overview?.marketPrice.includes('.') ? Number(overview?.marketPrice).toFixed(4) : Number(overview?.marketPrice)) || '0'}`,
         };
         console.log(contentMapping, "contentMapping")
         if (offeringDocumentList.length > 0) {
