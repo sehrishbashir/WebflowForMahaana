b6dbc170620f3f87cb144f33b418bddc43a36439
anasayubi-mahaana <anas.ayubi@mahaana.com>
2024-07-23 23:37:39 +0500

    Inclusion of single record query. Update miietf.js

--- miietf.js
+++ miietf.js
@@ -8534,7 +8534,7 @@ async function calcPerf() {
     let earliest_record = null
     let mtd = null
 
-    latest_record = airtable_single_record("desc")
+    latest_record = await airtable_single_record("desc", null)
     // await base('Adjust_nav_values').select({
     //     maxRecords: 1,
     //     view: "Grid view",
@@ -8551,20 +8551,22 @@ async function calcPerf() {
     d = new Date(latest_record.date)
     month_str = d.toLocaleString('en-GB', {month: '2-digit'})
     console.log(month_str)
-    
-    await base('Adjust_nav_values').select({
-        maxRecords: 1,
-        view: "Grid view",
-        filterByFormula: `IF({date} < '2024-${month_str}-01', 1, 0)`,
-        sort: [{field: "date", direction: "desc"}]
-    }).eachPage(function page(records, fetchNextPage) {
-    // This function (`page`) will get called for each page of records.
-        records.forEach(function (record) {
-            day_before_curr_month_record = record.fields
-        })
 
-        fetchNextPage()
-    }) 
+    filter_cond = `IF({date} < '2024-${month_str}-01', 1, 0)`
+    day_before_curr_month_record = await airtable_single_record("desc", filter_cond)
+    // await base('Adjust_nav_values').select({
+    //     maxRecords: 1,
+    //     view: "Grid view",
+    //     filterByFormula: `IF({date} < '2024-${month_str}-01', 1, 0)`,
+    //     sort: [{field: "date", direction: "desc"}]
+    // }).eachPage(function page(records, fetchNextPage) {
+    // // This function (`page`) will get called for each page of records.
+    //     records.forEach(function (record) {
+    //         day_before_curr_month_record = record.fields
+    //     })
+
+    //     fetchNextPage()
+    // }) 
 
     console.log('latest_record')
     console.log(latest_record)
@@ -8573,18 +8575,19 @@ async function calcPerf() {
     if (day_before_curr_month_record !== null) {
         mtd = latest_record.navValue / day_before_curr_month_record.navValue - 1    
     } else {
-        await base('Adjust_nav_values').select({
-            maxRecords: 1,
-            view: "Grid view",
-            sort: [{field: "date", direction: "asc"}]
-        }).eachPage(function page(records, fetchNextPage) {
-        // This function (`page`) will get called for each page of records.
-            records.forEach(function (record) {
-                earliest_record = record.fields
-            })
+        earliest_record = await airtable_single_record("asc", null)
+        // await base('Adjust_nav_values').select({
+        //     maxRecords: 1,
+        //     view: "Grid view",
+        //     sort: [{field: "date", direction: "asc"}]
+        // }).eachPage(function page(records, fetchNextPage) {
+        // // This function (`page`) will get called for each page of records.
+        //     records.forEach(function (record) {
+        //         earliest_record = record.fields
+        //     })
             
-            fetchNextPage()
-        })
+        //     fetchNextPage()
+        // })
 
         mtd = latest_record.navValue / earliest_record.navValue - 1
     }
