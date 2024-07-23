03dd8b714f659a998b139e6a87fcbe25f3875dac
anasayubi-mahaana <anas.ayubi@mahaana.com>
2024-07-23 21:22:27 +0500

    Update script.js

--- script.js
+++ script.js
@@ -27,16 +27,6 @@ if (mixpanel) {
 // ---------------------------------------------- //
 
 
-// ---------------- HELP CRUNCH ---------------- //
-const org = HC_ORGANIZATION;
-const appId = HC_APPID;
-const userData = { custom_data: { approch: 'Website' } }
-
-window.helpcrunchSettings = { organization: org, appId: appId, user: userData };
-(function (w, d) { var hS = w.helpcrunchSettings; if (!hS || !hS.organization) { return; } var widgetSrc = 'https://' + hS.organization + '.widget.helpcrunch.com/'; w.HelpCrunch = function () { w.HelpCrunch.q.push(arguments) }; w.HelpCrunch.q = []; function r() { if (d.querySelector('script[src="' + widgetSrc + '"')) { return; } var s = d.createElement('script'); s.async = 1; s.type = 'text/javascript'; s.src = widgetSrc; (d.body || d.head).appendChild(s); } if (d.readyState === 'complete' || hS.loadImmediately) { r(); } else if (w.attachEvent) { w.attachEvent('onload', r) } else { w.addEventListener('load', r, false) } })(window, document)
-// ---------------------------------------------- //
-
-
 // ---------------- SCROLL TO TOP ---------------- //
 function scrollToTop() { window.scroll({ top: 0, behavior: 'smooth' }) }
 // window.addEventListener('unload', () => scrollToTop());
