6dd64a182b391aca15156ddd261a7ca11e5c1179
anasayubi-mahaana <anas.ayubi@mahaana.com>
2024-07-24 15:18:01 +0500

    Update miietf.js

--- miietf.js
+++ miietf.js
@@ -8593,7 +8593,7 @@ async function calcPerf() {
     // let curr_FY_start_str = curr_FY_start.toLocaleString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit'})
     // let curr_FY_start_str = curr_FY_start.toISOString().slice(0, 10);
     let curr_FY_start_str = format_date(curr_FY_start)
-    console.log(curr_FY_start.toISOString()) 
+    console.log(curr_FY_start_str) 
     console.log('curr_FY_start_str')
     console.log(curr_FY_start_str)
 
@@ -8614,10 +8614,9 @@ async function calcPerf() {
 
 // convert date to this format: 2024-11-01 
 function format_date(d) {
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate()).padStart(2, '0');
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const day = String(d.getDate()).padStart(2, '0');
     const formattedDate = `${year}-${month}-${day}`; // Output: "2024-07-24"
     return formattedDate
 }
