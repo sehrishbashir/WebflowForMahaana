59676c977db83b677217b197c951d98e617f214c
Ashar Nadeem <ashar.nadeem@Mahaana.com>
2025-06-19 17:40:24 +0500

    url change

--- miietf.js
+++ miietf.js
@@ -513,7 +513,7 @@ async function getAppWriteData(productName) {
             else if (productName === 'MIIRF') {
                 console.log('Fetching MIIRF data');
                 // appw_data = await fetch('https://dev-mahaana-wealth-marketfeedinvestuniverse.azurewebsites.net/api/fund-data/miirf?')
-                appw_data = await fetchWithTimeout('https://dev-mahaana-wealth-marketfeedinvestuniverse.azurewebsites.net/api/fund-data/miirf', timeoutDuration);
+                appw_data = await fetchWithTimeout('https://dev-mahaana-wealth-marketfeedinvestuniverse.azurewebsites.net/api/fund-data?name=miirf', timeoutDuration);
             }
 
             // Check if the response is successful
